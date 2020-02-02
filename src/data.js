2ae2140aa1e0eae6900660b7bfc77da1eccf233b
James Ku <jameskutw@gmail.com>
2020-02-02 18:45:00 -0500

    Update timeline data

--- src/data.js
+++ src/data.js
@@ -1,51 +1,75 @@
 const data = [
     {
-        text: 'Started my website',
-        date: 'March 03 2018',
+        text: 'Started blogging',
+        date: 'Dec 03 2019',
         category: {
-            tag: 'medium',
-            color: '#018f69'
+            tag: 'website',
+            color: '#3282B8'
         },
         link: {
-            url: 'jamesku.xyz',
-            text: 'Read more'
+            url: 'https://jamesku.xyz/posts',
+            text: 'Visit my website'
         }
     },
     {
         text: 'Started my website',
-        date: 'March 03 2018',
+        date: 'Sep 23 2019',
         category: {
-            tag: 'medium',
-            color: '#018f69'
+            tag: 'website',
+            color: '#3282B8'
         },
         link: {
-            url: 'jamesku.xyz',
-            text: 'Read more'
+            url: 'https://jamesku.xyz',
+            text: 'Visit my website'
         }
     },
     {
-        text: 'Started my website',
-        date: 'March 03 2018',
+        text: 'Started graduate school at University of Virginia',
+        date: 'Aug 27 2019',
         category: {
-            tag: 'medium',
-            color: '#018f69'
+            tag: 'school',
+            color: '#A278B5'
         },
-        link: {
-            url: 'jamesku.xyz',
-            text: 'Read more'
-        }
     },
     {
-        text: 'Started my website',
-        date: 'March 03 2018',
+        text: 'Ended job at Aurora Tech',
+        date: 'July 15 2019',
         category: {
-            tag: 'medium',
-            color: '#018f69'
+            tag: 'career',
+            color: '#ffc55c'
         },
-        link: {
-            url: 'jamesku.xyz',
-            text: 'Read more'
-        }
+    },
+    {
+        text: 'Started job at Aurora Tech',
+        date: 'Nov 10 2018',
+        category: {
+            tag: 'career',
+            color: '#ffc55c'
+        },
+    },
+    {
+        text: 'Graduated from National Tsing Hua University',
+        date: 'Jun 23 2018',
+        category: {
+            tag: 'school',
+            color: '#A278B5'
+        },
+    },
+    {
+        text: 'Started school at National Tsing Hua University',
+        date: 'Sep 1 2014',
+        category: {
+            tag: 'school',
+            color: '#A278B5'
+        },
+    },
+    {
+        text: 'Born',
+        date: 'May 28 1996',
+        category: {
+            tag: 'life',
+            color: '#ee8972'
+        },
     }
 ]
 
